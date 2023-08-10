c6fac8a6f858aaf48dccd90e765e4f6e956524de
windeer9 <young7362@gmail.com>
2023-08-10 17:01:22 +0900

    commit

---
+++ server/src/main/java/com/comment/comment.java
@@ -0,0 +1,12 @@
+package server.src.main.java.com.comment;
+
+public class comment {
+
+    private int id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+}
